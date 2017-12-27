f9482671203da236a59fe713ca4909e936b3ab12
jacquetta <jacquettavb@hotmail.com>
2017-12-26 22:58:58 -0600

    revised commit

--- app.js
+++ app.js
@@ -1,50 +1,23 @@
 function onReady() {
-  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
+  const addToDoForm = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
-  function createNewToDo() {
-    if(!newToDoText.value) { return; }
-
-    toDos.push({
-      title: newToDoText.value,
-      complete: false
-    });
-
-    newToDoText.value = '';
-
-    renderTheUI();
-  }
-
-  function renderTheUI() {
-    const toDoList = document.getElementById('toDoList');
-
-    toDoList.textContent = '';
-
-    toDos.forEach(function(toDo){
-      const newLi = document.createElement('li');
-      const checkbox = document.createElement('input');
-      checkbox.type = "checkbox";
-
-      const title = document.createElement('span');
-      newLi.textContent = toDo.title;
-
-      toDoList.appendChild(newLi);
-      newLi.appendChild(checkbox);
-    });
-  }
-
-  addToDoForm.addEventListener('submit', event => {
+  addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
-    createNewToDo();
+
+    let title = newToDoText.value;
+    let newLi = document.createElement('li');
+    let checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    newLi.textContent = title;
+    newLi.appendChild(checkbox);
+    toDoList.appendChild(newLi);
     newToDoText.value = '';
   });
-
-  renderTheUI();
 }
 
-
 window.onload = function() {
+
   onReady();
 };
